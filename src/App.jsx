1100d948972881b2038ba2b4836cab1ecbbf1fb7
James Cameron <jamiecam43@gmail.com>
2026-04-28 16:57:22 -0400

    added error handling to movie searches

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,42 @@ import MoviesList from './components/MoviesList';
 import Box from './components/Box';
 import WatchedSummary from './components/WatchedSummary';
 import WatchedMoviesList from './components/WatchedMoviesList';
+import Loader from './components/Loader';
+import ErrorMessage from './components/ErrorMessage';
 
 const KEY = 'f82d5389';
 
 export default function App() {
 	const [movies, setMovies] = useState([]);
 	const [watched, setWatched] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState('');
+	const query = 'weriid';
 
 	useEffect(function () {
 		const getMovies = async function () {
-			const res = await fetch(
-				`https://www.omdbapi.com/?apikey=${KEY}&s=castaway`,
-			);
-			const movies = await res.json();
-			setMovies(movies.Search);
+			try {
+				setIsLoading(true);
+				const res = await fetch(
+					`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+				);
+
+				if (!res.ok)
+					throw new Error('Something went wrong with the fetching of movies');
+
+				const data = await res.json();
+
+				if (data.Response === 'False')
+					throw new Error(
+						'We could not find any movies that matched your search',
+					);
+				setMovies(data.Search);
+				console.log(data);
+			} catch (err) {
+				setError(err.message);
+			} finally {
+				setIsLoading(false);
+			}
 		};
 		getMovies();
 	}, []);
@@ -34,7 +56,9 @@ export default function App() {
 			</NavBar>
 			<main className='main'>
 				<Box>
-					<MoviesList movies={movies} />
+					{isLoading && <Loader />}
+					{!isLoading && !error && <MoviesList movies={movies} />}
+					{error && <ErrorMessage message={error} />}
 				</Box>
 				<Box>
 					<WatchedSummary watched={watched} />
